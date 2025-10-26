6be7df5c3526fcc0214633508bce66410b0205bc
MaxSC4 <maxime.soarescorreia@gmail.com>
2025-10-26 11:54:43 +0000

    Deploying to gh-pages from @ MaxSC4/maxsc4.github.io@bf504935bac600723f206f8595289a60047b3f29 🚀

--- assets/js/giscus-setup.js
+++ assets/js/giscus-setup.js
@@ -18,7 +18,7 @@ function determineGiscusTheme() {
 
   let giscusAttributes = {
     src: "https://giscus.app/client.js",
-    "data-repo": "MaxSC4/maximesc.github.io",
+    "data-repo": "MaxSC4/maxsc4.github.io",
     "data-repo-id": "",
     "data-category": "Comments",
     "data-category-id": "",
